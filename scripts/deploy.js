a8365e201c6b50409791237ff16f85b925fa1108
GarryFCR <garryranee@gmail.com>
2021-05-27 23:26:00 +0530

    Planning

---
+++ scripts/deploy.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const data = require('./list.json');
+
+async function main(){
+
+fs.writeFileSync('../src/js/app.js', JSON.stringify(data));
+}
+
+main().then(()=> process.exit(0))
+	.catch(error=>{
+		console.error(error);
+		process.exit(1);
+	});
