58d69021072672a6c332653049e5c4ee4c7766b4
GarryFCR <garryranee@gmail.com>
2021-06-05 01:25:35 +0530

    Deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,20 @@
 const fs = require('fs');
-const data = require('./list.json');
+const data = require('./root.json');
+const root=data.Merkle_root[0];
+
 
 async function main(){
+
+	const [owner] =  await ethers.getSigners();
+	console.log("Owner:",owner.address);
+
+	const Token = await ethers.getContractFactory("Alphacointract");
+  	const token = await Token.deploy();
+
+  	const Dist = await ethers.getContractFactory("Merkle_Distributor");
+  	const dist = await Dist.deploy(token.address,root);
+
+  	console.log("Merkle Distributor contract :",dist.address);
 
 
 }
