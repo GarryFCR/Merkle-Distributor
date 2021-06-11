5dafcbb7c1c6fb32706391ce6b934702704c0091
GarryFCR <garryranee@gmail.com>
2021-06-11 23:50:00 +0530

    Connecting to Metamask

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css';
-import Chain from "./component.jsx";
+import App from "./component.js";
 
-ReactDom.render(<Chain/>, document.getElementById("root"));
+ReactDom.render(<App/>, document.getElementById("root"));
